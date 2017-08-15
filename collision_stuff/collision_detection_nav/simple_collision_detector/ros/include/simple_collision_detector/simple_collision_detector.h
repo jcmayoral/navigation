4e5ca124c71763616fbd620d8b358152078b8dac
jcmayoral <jocamaba1989@gmail.com>
2017-08-15 17:45:03 +0200

    created a new plugin for fault_core :) Just needed to addapt it in the fault_tolerant_move_base Check State Machine in thesis draft

--- collision_stuff/collision_detection_nav/simple_collision_detector/ros/include/simple_collision_detector/simple_collision_detector.h
+++ collision_stuff/collision_detection_nav/simple_collision_detector/ros/include/simple_collision_detector/simple_collision_detector.h
@@ -1,26 +1,26 @@
-#ifndef SIMPLE_RECOVERY_BEHAVIOR_H
-#define SIMPLE_RECOVERY_BEHAVIOR_H
+#ifndef SIMPLE_COLLISION_DETECTOR_H
+#define SIMPLE_COLLISION_DETECTOR_H
 
 #include <ros/ros.h>
-#include <nav_core/recovery_behavior.h>
+#include <fault_core/fault_detector.h>
 #include <base_local_planner/costmap_model.h>
 
-namespace simple_recovery_behavior
+namespace simple_collision_detector
 {
 
-  class SimpleRecoveryBehavior : public nav_core::RecoveryBehavior
+  class SimpleCollisionDetector : public fault_core::FaultDetector
   {
     public:
 
       /**
        * @brief Constructor
        */
-      SimpleRecoveryBehavior();
+      SimpleCollisionDetector();
 
       /**
        * @brief Destructor
        */
-      ~SimpleRecoveryBehavior();
+      ~SimpleCollisionDetector();
 
       /**
        * @brief Initializes plugin
@@ -34,12 +34,12 @@ namespace simple_recovery_behavior
         costmap_2d::Costmap2DROS* localCostmapROS);
 
       /**
-       * @brief Executes the recovery behavior
+       * @brief Executes the car maneuver recovery behavior
        */
-      void runBehavior();
+      void detectFault();
 
   };
 
-}  // namespace simple_recovery_behavior
+}  // namespace simple_collision_detector
 
-#endif  // SIMPLE_RECOVERY_BEHAVIOR_H
+#endif  // SIMPLE_COLLISION_DETECTOR_H
---
+++ collision_stuff/collision_detection_nav/simple_collision_detector/ros/src/simple_collision_detector.cpp
@@ -0,0 +1,36 @@
+#include "simple_collision_detector/simple_collision_detector.h"
+#include <pluginlib/class_list_macros.h>
+
+// register this class as a recovery behavior plugin
+PLUGINLIB_DECLARE_CLASS(simple_collision_detector, SimpleCollisionDetector,
+                        simple_collision_detector::SimpleCollisionDetector,
+                        fault_core::FaultDetector)
+
+namespace simple_collision_detector
+{
+
+  SimpleCollisionDetector::SimpleCollisionDetector()
+  {
+    ROS_INFO("Constructor SimpleCollisionDetector");
+  }
+
+
+  SimpleCollisionDetector::~SimpleCollisionDetector()
+  {
+
+  }
+
+
+  void SimpleCollisionDetector::initialize(std::string name,
+    tf::TransformListener* tfListener,
+    costmap_2d::Costmap2DROS* globalCostmapROS,
+    costmap_2d::Costmap2DROS* localCostmapROS)
+  {
+
+  }
+
+  void SimpleCollisionDetector::detectFault()
+  {
+  }
+
+}  // namespace simple_recovery_behavor
