26d80558d21750c5feaa3cb5dfef9a63ac8c4e34
jcmayoral <jocamaba1989@gmail.com>
2018-01-09 16:06:49 +0100

    according to me this is the closest implementation of a kalman filter with the current implementation

--- collision_detection_nav/collision_detector_diagnoser/ros/include/collision_detector_diagnoser/sensor_fusion_methods.hpp
+++ collision_detection_nav/collision_detector_diagnoser/ros/include/collision_detector_diagnoser/sensor_fusion_methods.hpp
@@ -30,16 +30,16 @@ class ConsensusApproach : public SensorFusionApproach {
         ROS_DEBUG("Consensus");
         int counter = 0;
         for (std::list<fusion_msgs::sensorFusionMsg>::iterator it=v.begin(); it != v.end(); ++it){
+          if (counter >= v.size()*threshold){
           if(it->msg == fusion_msgs::sensorFusionMsg::ERROR){
             ROS_WARN_STREAM("Collision DETECTED on " << it->sensor_id);
             counter ++;
           }
+          }
         }
         ROS_DEBUG_STREAM("Counter " << counter);
-        if (counter >= v.size()*threshold){
           return true;
 
-        }
 
         return false;
       };
@@ -68,18 +68,56 @@ class WeightedApproach : public SensorFusionApproach {
 
 class KalmanFilterApproach : public SensorFusionApproach {
     public:
+
       bool detect(list<fusion_msgs::sensorFusionMsg> v){
         ROS_DEBUG("Kalman Filter");
-        std::vector<float> x(v.size());
+        // Based on http://www.ece.montana.edu/seniordesign/archive/SP14/UnderwaterNavigation/kalman_filter.html
+        std::vector<float> z(v.size());
         for (std::list<fusion_msgs::sensorFusionMsg>::iterator it=v.begin(); it != v.end(); ++it){
           for(int i = 0; i < it->data.size(); i++){
-            x[i] = it->data[i]; // initialize x (measured)
+            z[i] = it->data[i]; // initialize x (measured)
+            p[i] = it->data[i]; // TODO this part just simplifies covariance matrix but it must be calculated.
+                                // one variance for all sensors or i*i covariance
           }
         }
-        p = x;
+
+        x.clear();
+        y.clear();
+        k.clear();
+
+        for(int i = 0; i < z.size(); i++){
+          x.push_back(threshold);
+          y.push_back(z[i] - p[i]); // innovation
+          k.push_back(1);
+        }
+
+
+        for (auto i = y.begin(); i != y.end(); ++i){
+          std::cout << *i << ' ';
+        }
+
+        s = p; // H = 1
+
+
+        for(int i = 0; i < x.size(); i++){
+          x[i] = x[i] + k[i]*y[i]; // assuming error should be constant x_p = A x(n-1) + B u(n)
+        }
+
+        for (auto j = k.begin(); j != k.end(); ++j){
+          if (*j > threshold){
+            return true;
+          }
+        }
+
         return false;
       };
 
     private:
-      std::vector<float> p;
+      std::vector<float> x;//state prediction
+      std::vector<float> y;//innovation
+      std::vector<float> k;//filter gain
+      std::vector<float> p;//covariance prediction
+      std::vector<float> s;// innovation covariance
+      std::vector<float> q;//noise TODO 0 for now
+
 };
